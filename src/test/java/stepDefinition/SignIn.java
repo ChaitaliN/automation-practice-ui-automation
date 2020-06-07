5945d9193093812aa5e25e55cf827255d1e41479
ChaitaliN <chaitu.shinde18@gmail.com>
2020-06-07 21:08:55 +1000

    Added WebDriverManager code

--- src/test/java/stepDefinition/SignIn.java
+++ src/test/java/stepDefinition/SignIn.java
@@ -1,10 +1,10 @@
 package stepDefinition;
 
-import cucumber.api.java.After;
-import cucumber.api.java.Before;
-import cucumber.api.java.en.Given;
-import cucumber.api.java.en.Then;
-import cucumber.api.java.en.When;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import driver.Driver;
 import webpage.SignInPage;
 
